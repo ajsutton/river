be9ee6152dd8bbab8dd50c1da1b7275b697bae2c
Adrian Sutton <adrian@symphonious.net>
2015-09-22 13:14:32 +1000

    Reduce verbosity of messages.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,13 +8,11 @@ class SessionsController < ApplicationController
     @identity = Identity.find_with_omniauth(auth)
 
     if @identity.nil?
-      logger.info("Creating new identity")
       # If no identity was found, create a brand new one here
       @identity = Identity.create_with_omniauth(auth)
     end
 
     if signed_in?
-      logger.info("signed in")
       if @identity.user == current_user
         logger.info("Already signed in with same identity")
         # User is signed in so they are trying to link an identity with their
@@ -37,7 +35,7 @@ class SessionsController < ApplicationController
         # The identity we found had a user associated with it so let's 
         # just log them in here
         self.current_user = @identity.user
-        redirect_to root_url, notice: "Signed in!"
+        redirect_to root_url
       else
         logger.info("No user")
         # No user associated with the identity so we need to create a new one
@@ -46,13 +44,13 @@ class SessionsController < ApplicationController
         @identity.user = @user
         @identity.save!
         self.current_user = @user
-        redirect_to new_church_path, notice: "Welcome, please set up your church."
+        redirect_to new_church_path
       end
     end
   end
 
   def destroy
     self.current_user = nil
-    redirect_to root_url, notice: "Logged out."
+    redirect_to root_url
   end
 end

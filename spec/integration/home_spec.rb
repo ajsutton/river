16b291d713e9f4b3625d29530f45aa91c93210fc
Adrian Sutton <adrian@symphonious.net>
2015-09-23 21:00:00 +1000

    More acceptance tests.

--- spec/integration/home_spec.rb
+++ spec/integration/home_spec.rb
@@ -5,4 +5,23 @@ describe 'home page' do
     visit('/')
     expect(page).to have_content('Welcome')
   end
+  
+  it 'displays a login button when not logged in' do
+    visit('/')
+    expect(find('.jumbotron')).to have_css('.login')
+    expect(find('.navbar')).to have_css('.login')
+  end
+  
+  it 'displays a logout button when not logged in' do
+    login
+    expect(find('.navbar')).to have_css('.logout')
+  end
+  
+  def login
+    visit('/')
+    find('.navbar .login').click
+    fill_in 'name', :with => 'John Doe'
+    fill_in 'email', :with => 'john@doe.com'
+    click_button 'Sign In'
+  end
 end

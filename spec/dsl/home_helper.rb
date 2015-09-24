11dbfc2ecb935108b0acbccaef8039af145fb7a9
Adrian Sutton <adrian@symphonious.net>
2015-09-24 18:14:05 +1000

    Wrap HomeDriver in the Dsl module.

--- spec/dsl/home_helper.rb
+++ spec/dsl/home_helper.rb
@@ -1,31 +1,33 @@
-class HomeDriver
-  include Capybara::DSL
-  include ::RSpec::Matchers
+module Dsl
+  class HomeDriver
+    include Capybara::DSL
+    include ::RSpec::Matchers
 
-  def login(options = {})
-    params = Dsl.params(options, {
-      name: 'John Doe',
-      email: 'john@example.com'
-    })
-    visit('/')
-    find('.navbar .login').click
-    fill_in 'name', :with => params[:name]
-    fill_in 'email', :with => params[:email]
-    click_button 'Sign In'
-  end
+    def login(options = {})
+      params = Dsl.params(options, {
+        name: 'John Doe',
+        email: 'john@example.com'
+      })
+      visit('/')
+      find('.navbar .login').click
+      fill_in 'name', :with => params[:name]
+      fill_in 'email', :with => params[:email]
+      click_button 'Sign In'
+    end
   
-  def logout
-    visit('/')
-    find('.navbar .logout').click
-  end
+    def logout
+      visit('/')
+      find('.navbar .logout').click
+    end
   
-  def login_present!
-    visit('/')
-    expect(find('.jumbotron')).to have_css('.login')
-    expect(find('.navbar')).to have_css('.login')
-  end
+    def login_present!
+      visit('/')
+      expect(find('.jumbotron')).to have_css('.login')
+      expect(find('.navbar')).to have_css('.login')
+    end
   
-  def logout_present!
-    expect(find('.navbar')).to have_css('.logout')
+    def logout_present!
+      expect(find('.navbar')).to have_css('.logout')
+    end
   end
 end

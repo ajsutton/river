9b218975902f5aa8695d5d061e511fdf0532c420
Adrian Sutton <adrian@symphonious.net>
2015-09-25 09:33:48 +1000

    Remove unique index on users.name.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150922044233) do
+ActiveRecord::Schema.define(version: 20150924233016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +42,6 @@ ActiveRecord::Schema.define(version: 20150922044233) do
   end
 
   add_index "users", ["church_id"], name: "index_users_on_church_id", using: :btree
-  add_index "users", ["name"], name: "index_users_on_name", unique: true, using: :btree
 
   add_foreign_key "users", "churches"
 end

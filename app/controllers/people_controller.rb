2c7a9d8e4e2c91392054d69af42e6d0d59afd761
Adrian Sutton <adrian@symphonious.net>
2015-09-25 21:11:08 +1000

    Add index view for people.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,6 +2,6 @@ class PeopleController < ApplicationController
   before_action :require_church!
   
   def index
-    @people = Person.find_by(church: current_user.church)
+    @people = Person.where(church: current_user.church)
   end
 end

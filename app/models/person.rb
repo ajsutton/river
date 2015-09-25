bac9ce1ce23193bd6b41419349f8011bacd2ba5e
Adrian Sutton <adrian@symphonious.net>
2015-09-25 11:27:18 +1000

    Start fleshing out person model.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,9 @@
 class Person < ActiveRecord::Base
+  
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  
+  def name
+    return "#{first_name} #{last_name}"
+  end
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -2,10 +2,18 @@ require 'rails_helper'
 
 describe Person, type: :model do
   it 'should have a valid factory' do
-    expect(build(:person)).to be_valid
+    expect(create(:person)).to be_valid
   end
   
-  it 'requires first name'
-  it 'requires last name'
-  it 'creates a name from first and last names'
+  it 'requires first name' do
+    expect(build(:person, first_name: nil)).to be_invalid
+  end
+  
+  it 'requires last name' do
+    expect(build(:person, last_name: nil)).to be_invalid
+  end
+  
+  it 'creates a name from first and last names' do
+    expect(build(:person, first_name: 'Roger', last_name: 'Smith').name).to eq('Roger Smith')
+  end
 end

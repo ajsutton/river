f76be5c450db715ebd01516521e3e086f50938b6
Adrian Sutton <adrian@symphonious.net>
2015-09-26 23:14:55 +1000

    Use rails logger for omniauth.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  OmniAuth.config.logger = Rails.logger
   provider :developer unless Rails.env.production?
   # other providers here
 end

59037e9550ad7fe21bd6f4e060014d6e6ffa0aae
Adrian Sutton <adrian@symphonious.net>
2015-10-02 21:29:57 +1000

    Move custom field methods to a concern.

---
+++ app/models/concerns/custom_fields.rb
@@ -0,0 +1,36 @@
+module CustomFields extend ActiveSupport::Concern
+    included do
+        after_initialize :init
+    end
+
+    def get_field(name)
+        id = field_schema.id_for_name(name)
+        self.fields[id]
+    end
+
+    def set_field(name, value)
+        id = field_schema.id_for_name(name)
+        self.fields[id] = value
+    end
+
+    def fields_from_params(params)
+        field_definitions.each do |field|
+            set_field(field[:name], params[field[:id]])
+        end
+    end
+
+    def field_definitions
+        field_schema.fields
+    end
+
+    private
+
+    def init
+      self.fields = {} if self.fields.nil?
+    end
+
+    def field_schema
+        @schema ||= FieldSchema.find_by(church: self.church, applies_to: self.class.name.downcase)
+        @schema || FieldSchema.new(church: church, applies_to: self.class.name.downcase)
+    end
+end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,44 +1,14 @@
 # -*- encoding : utf-8 -*-
 class Person < ActiveRecord::Base
-  belongs_to :church
+    include CustomFields
+    belongs_to :church
 
-  validates :church, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+    validates :church, presence: true
+    validates :first_name, presence: true
+    validates :last_name, presence: true
 
-  after_initialize :init
 
-  def name
-    "#{first_name} #{last_name}"
-  end
-
-  def get_field(name)
-      id = field_schema.id_for_name(name)
-      self.fields[id]
-  end
-
-  def set_field(name, value)
-      id = field_schema.id_for_name(name)
-      self.fields[id] = value
-  end
-
-  def fields_from_params(params)
-      field_definitions.each do |field|
-          set_field(field[:name], params[field[:id]])
-      end
-  end
-
-  def field_definitions
-      field_schema.fields
-  end
-
-  private
-  def init
-    self.fields = {} if self.fields.nil?
-  end
-
-  def field_schema
-      @schema ||= FieldSchema.find_by(church: self.church, applies_to: 'person')
-      @schema || FieldSchema.new(church: church, applies_to: 'person')
-  end
+    def name
+        "#{first_name} #{last_name}"
+    end
 end

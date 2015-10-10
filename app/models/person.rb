cfa63e336fd82d167cb6fc2d04eccdc83df23c4c
Adrian Sutton <adrian@symphonious.net>
2015-10-11 07:00:56 +1000

    Make conversion to sql operator and value more readable.

--- app/models/person.rb
+++ app/models/person.rb
@@ -7,43 +7,14 @@ class Person < ActiveRecord::Base
     validates :first_name, presence: true
     validates :last_name, presence: true
 
-    def Person.where_view(view)
+    def self.where_view(view)
         matches = Person.where(church: view.church)
         if !view.filters.nil?
             view.filters.each do |rule|
                 condition = rule['condition']
                 field_name = condition['field']
-                value = condition['filterValue'] ? condition['filterValue'][0] : nil
-                case condition['operator']
-                when 'equal'
-                    operator = '='
-                when 'begins_with'
-                    operator = 'LIKE'
-                    value = "#{value}%"
-                when 'not_begins_with'
-                    operator = 'NOT LIKE'
-                    value = "#{value}%"
-                when 'contains'
-                    operator = 'LIKE'
-                    value = "%#{value}%"
-                when 'not_contains'
-                    operator = 'NOT LIKE'
-                    value = "%#{value}%"
-                when 'ends_with'
-                    operator = 'LIKE'
-                    value = "%#{value}"
-                when 'not_ends_with'
-                    operator = 'NOT LIKE'
-                    value = "%#{value}"
-                when 'is_empty'
-                    operator = '='
-                    value = ''
-                when 'not_is_empty'
-                    operator = '!='
-                    value = ''
-                else
-                    throw "invalid operator #{operator}"
-                end
+                operator = as_sql_operator condition['operator']
+                value = as_sql_value(condition['operator'], condition['filterValue'] ? condition['filterValue'][0] : nil)
 
                 if column_names.include? field_name
                     matches = matches.where("#{field_name} #{operator} ?", value)
@@ -58,4 +29,36 @@ class Person < ActiveRecord::Base
     def name
         "#{first_name} #{last_name}"
     end
+
+    def self.as_sql_operator(condition_operator)
+        case condition_operator
+        when 'equal', 'is_empty'
+            '='
+        when 'begins_with', 'contains', 'ends_with'
+            'LIKE'
+        when 'not_begins_with', 'not_contains', 'not_ends_with'
+            'NOT LIKE'
+        when 'not_equal', 'not_is_empty'
+            '!='
+        else
+            throw 'Invalid operator'
+        end
+    end
+    private_class_method :as_sql_operator
+
+    def self.as_sql_value(condition_operator, raw_value)
+        case condition_operator
+        when 'begins_with', 'not_begins_with'
+            "#{raw_value}%"
+        when 'contains', 'not_contains'
+            "%#{raw_value}%"
+        when 'ends_with', 'not_ends_with'
+            "%#{raw_value}"
+        when 'is_empty', 'not_is_empty'
+            ''
+        else
+            raw_value
+        end
+    end
+    private_class_method :as_sql_value
 end
